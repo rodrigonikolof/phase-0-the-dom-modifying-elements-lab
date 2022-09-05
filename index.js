ebfe797e2a8469515ee63ae786b87682d2688998
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-09-05 10:10:08 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const mainHTML = document.querySelector("main");
+mainHTML.remove();
+
+const newHeader = document.createElement("H1");
+newHeader.id = 'victory';
+newHeader.textContent = 'is the champion';
+document.body.append(newHeader);
+
