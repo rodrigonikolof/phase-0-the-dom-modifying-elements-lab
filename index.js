84e8d02da70a6c740e6350f54f1d0b46637ecaf1
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-10-01 11:54:37 +1000

    commit tree 03f307f2c165bb4f8fc86d7c9168508e8a288361

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-const mainHTML = document.querySelector("main");
-mainHTML.remove();
+// const mainHTML = document.querySelector("main");
+// mainHTML.remove();
 
-const newHeader = document.createElement("H1");
+// const newHeader = document.createElement("H1");
+// newHeader.id = 'victory';
+// newHeader.textContent = 'is the champion';
+// document.body.append(newHeader);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const mainThing = document.querySelector('#main');
+mainThing.remove();
+const newHeader = document.createElement('h1');
 newHeader.id = 'victory';
-newHeader.textContent = 'is the champion';
-document.body.append(newHeader);
+newHeader.textContent = 'is the champion!'
+document.body.append(newHeader)
+
+
+
+
+
+
+
 
